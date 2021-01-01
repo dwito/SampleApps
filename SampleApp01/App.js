d9f24e94681090ec42b0c2fd8213321fdb4e34f2
dwito <t-ito@deltawing.co.jp>
2021-01-01 17:28:13 +0900

    別クラスにViewパーツ宣言して値を渡して表示する

--- SampleApp01/App.js
+++ SampleApp01/App.js
@@ -32,51 +32,67 @@ import {
 export default class App extends Component {
 
 //コンストラクラでstateを初期化
-constructor(props){
-  super(props)
-  this.state = {
-    message:""
+  constructor(props){
+    super(props)
+    this.state = {
+      message:"default"
+    }
   }
-}
 
   render() {
-    let msg = "Hello World!!"
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <View　style = {style.base}>
-          <Text>
-            {msg}
-          </Text>
-          {/* Buttonは閉じカッコが他と違うので注意 */}
-          <Button title="Click!" onPress={this.doAction}/>
-          <Text>
-            {this.state.message}
-          </Text>
-          {/* <Button title="Change message!" onPress={this.doChangeAction}/> */}
-          <Button title="Change message!" onPress={this.doChangeAction}/>
-        </View>
-      </SafeAreaView>
-    </>
-  );
+      let msg = "Hello World!!"
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <View　style = {style.base}>
+            <Text>
+              {msg}
+            </Text>
+            {/* Buttonは閉じカッコが他と違うので注意 */}
+            <Button title="Click!" onPress={this.doAction}/>
+            <Text>
+              {this.state.message}
+            </Text>
+            <Button title="Change message!" onPress={this.doChangeAction}/>
+            {/* MyViewの変数に値を設定 */}
+            <MyView name = "aaa"/>
+          </View>
+        </SafeAreaView>
+      </>
+    );
+  };//render
+
+  doAction = () => {
+    Alert.alert('you Clicked!');
+  }
+
+  doChangeAction = () => {
+    this.setState({message:"aaaa"});
+  }
 };
 
-doAction = () => {
-  Alert.alert('you Clicked!');
-}
+class MyView extends Component {
 
-doChangeAction = () => {
-  this.setState({message:"aaaa"});
-}
+  constructor(props){
+    super(props)
+    //いきなり定義して変数になるのに違和感を感じるが、これてグローバル変数として使えるようだ
+    props.name = "Hoge"
+  }
 
-};
+  render(){
+    return(
+      <Text style = {style.base}>別クラスにComponenで定義:{this.props.name}</Text>
+    );
+  }//render
+
+}//Component
 
 
 //constでスタイルを新規作成
 const style = StyleSheet.create({
 //View用のスタイル:base
-base: {
-  padding:50
-}
+  base: {
+    padding:50
+  }
 });
