ddc73405d2676e30c3e0f7ae9c1c1a3a554a190b
dwito <t-ito@deltawing.co.jp>
2021-01-02 17:13:32 +0900

    画面遷移するサンプルを実装

--- SampleApp03/App.js
+++ SampleApp03/App.js
@@ -5,39 +5,88 @@
  * @format
  * @flow strict-local
  */
-
+import 'react-native-gesture-handler';
 import React, {Component} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   View,
+  Button,
   Text,
   StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// 最新のnavigationはバージョンがV5であるため、教則本やネットは参考にならないため、公式を参考にした
+// 以下のインポートをするためには　以下のコマンドを実行2と4はなぜ必要なの意味がわからない（https://reactnavigation.org/docs/getting-started/）
+// npm install @react-navigation/native
+// expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+// npm install react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
+// npx pod-install ios
 
-export default class App extends Component {
-    render() {
-      return (
-        <>
-          <StatusBar barStyle="dark-content" />
-          <SafeAreaView>
-            <Text>aaaa</Text>
-          </SafeAreaView>
-        </>
-      );
-    };
-};
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const styles = StyleSheet.create({
-  body: {
-    backgroundColor: Colors.white,
-  },
-});
+
+function HomeScreen({ navigation }) {
+  return (
+    <View>
+      <Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Profile')}
+      />
+    </View>
+  );
+}
+
+function ProfileScreen({ navigation }) {
+  return (
+    <View>
+      <Button
+        title="Go to Notifications"
+        onPress={() => navigation.navigate('Notifications')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View>
+      <Button
+        title="Go to Settings"
+        onPress={() => navigation.navigate('Settings')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+function SettingsScreen({ navigation }) {
+  return (
+    <View>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Notifications" component={NotificationsScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
